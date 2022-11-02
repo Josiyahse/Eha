c09751c917d44c2d440fb51a872b5d7bd3c7fb49
Josias <koaffiyahse@gmail.com>
2022-11-02 19:38:33 +0100

    set up style

---
+++ src/style/composantStyle.js
@@ -0,0 +1,49 @@
+const Row = "flex flex-row ";
+
+const Column = "flex flex-col ";
+
+const Head1 = "text-5xl ";
+
+const Head3 = "text-3xl ";
+
+const Bold = "font-bold ";
+
+const JustifyContentCenter = "justify-center ";
+
+const JustifyContentSpaceBetween = "justify-between";
+
+const JustifyContentLeft = "justify-left ";
+
+const PrimaryColor = "text-[#3C3A2D] ";
+
+const BgRed = "bg-[#FFBBC2] ";
+
+const BgWith = "bg-[#FFFFFF] ";
+
+const BgGreen = "bg-[#A5F0AF] ";
+
+const Ronde = "rounded-full ";
+
+const Wrap = "flex-wrap ";
+
+const ItemCenter = "items-center  ";
+
+// const ="";
+
+export {
+  Row,
+  Column,
+  Head1,
+  Bold,
+  JustifyContentCenter,
+  PrimaryColor,
+  Head3,
+  BgWith,
+  BgGreen,
+  BgRed,
+  Ronde,
+  JustifyContentSpaceBetween,
+  Wrap,
+  ItemCenter,
+  JustifyContentLeft,
+};
